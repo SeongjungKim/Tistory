043cd4e2ee116b848c3f5742bf9bd37c2ab1d3ca
DavidKim <vder8282@gmail.com>
2022-05-24 18:28:52 +0900

    add sample code

---
+++ nRF52/zephyr_example/function/src/main.c
@@ -0,0 +1,16 @@
+#include <sys/printk.h>
+#include <zephyr.h>
+
+#include "myfunction.h"
+
+void main(void) {
+  int a = 3, b = 4;
+  while (1) {
+    if (!(IS_ENABLED(CONFIG_MYFUNCTION))) {
+      printk("MYFUNCTION not enabled\r\n");
+      return;
+    }
+    printk("The sum of %d and %d is %d\n\r", a, b, sum(a, b));
+    k_msleep(1000);
+  }
+}
---
+++ nRF52/zephyr_example/function/src/myfunction.c
@@ -0,0 +1,3 @@
+#include "myfunction.h"
+
+int sum(int a, int b) { return a + b; }
